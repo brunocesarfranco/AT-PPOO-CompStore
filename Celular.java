ad022e7b48450913c36cfbf0661f8617d830f6a9
Bruno Cesar Franco <brunocesarfranco@gmail.com>
2022-06-19 20:19:04 -0300

    Add prop tela to object Celular;
    Adicionado a propriedade tela para o objeto celular;

--- Celular.java
+++ Celular.java
@@ -1,9 +1,11 @@
 public class Celular extends Produtos { 
     private static final long serialVersionUID = 1L;
+    private int tela;
 
-public Celular(String modelo, String marca, String processador, int ram, String hd, int preco){
+public Celular(String modelo, String marca, String processador, int ram, String hd, int tela, int preco){
     super(modelo, marca, processador, ram, hd, preco);
-    this.tipoP= "Celular";
+    this.tipoP= "Notebook";
+    this.tela = tela;
 }
 
     @Override
@@ -12,8 +14,17 @@ public Celular(String modelo, String marca, String processador, int ram, String 
             "\nModelo: " + this.modelo + 
             "\nMarca: " + this.marca +
             "\nProcessador: " + this.processador +
-            "\nMemoria ram: " + this.ram +
+            "\nMemoria ram: " + this.ram + " gb" +
             "\nHD: " + this.hd +
+            "\nTela: " + this.tela + " pol" + 
             "\nPreço: " + this.preco;
     }   
+
+public void setTela(int tela){
+    this.tela = tela;
+}
+public int getTela(){
+    return tela;
+}
+
 }
